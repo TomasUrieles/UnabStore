d6ded836f5f132a5c2005fe4b75aef0b6911707c
Tomas Urieles <tomasurieles31@gmail.com>
2025-10-21 19:29:32 -0500

    Inicio firebase

--- app/src/main/java/co/edu/unab/me/tomasurielesunabstore/HomeScren.kt
+++ app/src/main/java/co/edu/unab/me/tomasurielesunabstore/HomeScren.kt
@@ -5,10 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ExitToApp
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,10 +19,15 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.ShoppingCart
 
 // Colores personalizados
 
 
+//private val icons: Any
 
 @Preview
 @OptIn(ExperimentalMaterial3Api::class)

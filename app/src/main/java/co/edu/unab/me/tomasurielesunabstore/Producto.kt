e236a94df1a2654e1295745607bf188edd1b768f
Tomas Urieles <tomasurieles31@gmail.com>
2025-11-06 22:48:17 -0500

    Implementacion de ProductoScreen y arreglo de errores sobre el camino

--- app/src/main/java/co/edu/unab/me/tomasurielesunabstore/Producto.kt
+++ app/src/main/java/co/edu/unab/me/tomasurielesunabstore/Producto.kt
@@ -1,8 +1,8 @@
 package co.edu.unab.me.tomasurielesunabstore
 
-class Producto(descripcion: String, nombre: String, precio: Double, id: String) {
-    val id: String? = null;
-    val nombre: String = "";
-    val descripcion: String = "";
-    val precio: Double = 0.0;
-}
+data class Producto(
+    val id: String? = null,
+    val nombre: String = "",
+    val descripcion: String = "",
+    val precio: Double = 0.0
+)
